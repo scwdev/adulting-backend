2cf9aef30d7e177178718eec7eed9b5f230397b7
Chris <Chris@margaritas-air.attlocal.net>
2021-08-20 12:18:28 -0700

    server.js connection.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log("helooooo")
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+// IMPORT DEPENDENCIES
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+// IMPORT DATABASE CONNECTION
+const mongoose = require("./db/connection");
+
+// CREATE EXPRESS APPLICATION OBJECT
+const app = express();
+
+// MIDDLEWARE
+app.use(cors()); 
+app.use(express.json()); 
+app.use(morgan("tiny")); 
+
+// ROUTES AND ROUTES
+app.get("/", (req, res) => res.send("Server is Working")); // <--- Route to test server
+
+// LISTENER
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
